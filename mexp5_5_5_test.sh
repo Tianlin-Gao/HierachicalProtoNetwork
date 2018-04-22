ea5309a70598c68c4ab6b8075e2d8945641cfc3a
12ds95 <957517653@qq.com>
2018-04-22 20:56:29 +0800

    Add setup and test shell script

---
+++ mexp5_5_5_test.sh
@@ -0,0 +1,2 @@
+python scripts/train/few_shot/run_train.py --data.train_episodes 3 --data.test_episodes 1 --train.learning_rate 0.001 --data.dataset=miniImagenet --model.x_dim=3,84,84 --data.way 5 --data.shot 5 --data.test_shot 5 --data.test_way 5 --log.exp_dir=results/m5_5way5shot 
+python scripts/predict/few_shot/run_eval.py --data.test_shot 5 --data.test_way 5 --model.model_path=results/m5_5way5shot/best_model.t7
---
+++ setup.sh
@@ -0,0 +1,6 @@
+cd scripts/train/few_shot
+ln -s ../../../protonets protonets
+ln -s ../../../data data
+cd ../../predict/few_shot 
+ln -s ../../../protonets protonets
+ln -s ../../../data data
