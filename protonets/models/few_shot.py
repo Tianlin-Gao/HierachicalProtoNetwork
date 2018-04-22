8ab16ca3446d8ed0e8bfdcd3cfc01a5ecd08e755
12ds95 <957517653@qq.com>
2018-04-22 20:55:17 +0800

    Delete mis track *.pyc file

--- protonets/models/few_shot.py
+++ protonets/models/few_shot.py
@@ -8,6 +8,7 @@ from protonets.models import register_model
 
 from .utils import euclidean_dist
 from visdom import Visdom
+import copy
 viz = Visdom()
 
 class Flatten(nn.Module):
@@ -18,10 +19,13 @@ class Flatten(nn.Module):
         return x.view(x.size(0), -1)
 
 class Protonet(nn.Module):
-    def __init__(self, encoder):
+    def __init__(self, shared_layers, corase_classifier, n_corase, fine_encoders):
         super(Protonet, self).__init__()
-        
-        self.encoder = encoder
+        self.register_buffer('shared_layers', shared_layers)
+        # self.shared_layers = shared_layers
+        self.corase_classifier = corase_classifier
+        self.n_corase = n_corase
+        self.fine_encoders = fine_encoders
 
     def loss(self, sample):
         xs = Variable(sample['xs']) # support
@@ -41,9 +45,21 @@ class Protonet(nn.Module):
         x = torch.cat([xs.view(n_class * n_support, *xs.size()[2:]),
                        xq.view(n_class * n_query, *xq.size()[2:])], 0)
 
-        z = self.encoder.forward(x)
-        # viz.text("z<br>"+str(z).replace("\n", "<br>"))
+
+        # share layers part
+        z_share = self.shared_layers.forward(x)
+
+        # corase classifier part
+        z_corase = self.corase_classifier.forward(z_share)
+
+        log_p_y_corase = F.log_softmax(z_corase)
+       
+        # fine feature part
+        z = self.fine_encoders[0].forward(z_share)
         z_dim = z.size(-1)
+
+        for i in range(1, self.n_corase):
+           z += self.fine_encoders[i].forward(z_share)
 
         z_proto = z[:n_class*n_support].view(n_class, n_support, z_dim).mean(1)
         zq = z[n_class*n_support:]
@@ -75,13 +91,54 @@ def load_protonet_conv(**kwargs):
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
+        
+    # shared_layers = nn.Sequential(
+    #     conv_block(x_dim[0], hid_dim),
+    #     conv_block(hid_dim, hid_dim),
+    #     conv_block(hid_dim, hid_dim),
+    # )
 
-    encoder = nn.Sequential(
-        conv_block(x_dim[0], hid_dim),
-        conv_block(hid_dim, hid_dim),
-        conv_block(hid_dim, hid_dim),
-        conv_block(hid_dim, z_dim),
+    model = torch.load('proto_results/m30_5way5shot/best_model.t7')
+
+    # load pretrained layers 
+    shared_layers = nn.Sequential(
+        copy.deepcopy(model.encoder[0]),
+        copy.deepcopy(model.encoder[1]),
+        copy.deepcopy(model.encoder[2])
+    )
+
+    for param in shared_layers.parameters():
+        param.requires_grad = False
+
+    # TODO: make n_corase a commandline parameter
+    n_corase = 1
+
+    def gap_block(in_channels, out_channels, pre_size):
+        return nn.Sequential(
+            nn.Conv2d(hid_dim, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.AvgPool2d(pre_size),
+        )
+
+    corase_classifier = nn.Sequential(
+        gap_block(hid_dim, n_corase, x_dim[1] // 8),
         Flatten()
     )
 
-    return Protonet(encoder)
+    fine_encoders = []
+    for i in range(n_corase):
+        fine_encoders.append(
+            nn.Sequential(
+                conv_block(hid_dim, z_dim),
+                Flatten()
+        ))
+    # encoder = nn.Sequential(
+    #     conv_block(x_dim[0], hid_dim),
+    #     conv_block(hid_dim, hid_dim),
+    #     conv_block(hid_dim, hid_dim),
+    #     conv_block(hid_dim, z_dim),
+    #     Flatten()
+    # )
+
+    return Protonet(shared_layers, corase_classifier, n_corase, fine_encoders)
